e33d62710cee53aa41b6a2aa3272f5d6d3b03564
triettruong <louistruong2001@gmail.com>
2022-10-14 18:02:10 +0700

    Grid with start and end node

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import Grid from './Components/Grid';
 
+const START_POS = [5, 10];
+const END_POS = [25, 10]
 const App: React.FC = () => {
-  let board: number[][] = [];
+  const mouseRef = useRef(false);
 
+
+  let board: number[][] = [];
   const populateBoard = () => {
-    for (let row = 0; row < 50; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        currentRow.push(col);
+    for (let col = 0; col < 50; col++) {
+      const currentCol = [];
+      for (let row = 0; row < 25; row++) {
+        currentCol.push(row);
       }
-      board.push(currentRow);
+      board.push(currentCol);
     }
   };
 
   populateBoard();
 
+  const isType = (x: number, y: number) => {
+    const [START_X, START_Y] = START_POS;
+    const [END_X, END_Y] = END_POS;
+    let type: string = "";
+    (START_X == x && START_Y == y) ? type = "start" : "";
+    (END_X == x && END_Y == y) ? type = "end" : "";
+    return type;
+  }
+
   return (
     <main className='App'>
-      <div className="grid-container">
+      <div className="grid-container"
+        onMouseDown={() => {
+          mouseRef.current = true;
+        }}
+        onMouseUp={() => {
+          mouseRef.current = false;
+        }}
+      >
         {board.map((row, rowIndex) => {
-          return row.map((colIndex) => {
-            return <Grid coordinate={[rowIndex, colIndex]} />;
-          })
+          return (
+            <div key={rowIndex}>
+              {row.map((colIndex) => {
+                return <Grid
+                  coordinate={[rowIndex, colIndex]}
+                  type={isType(rowIndex, colIndex)}
+                  mouseRef={mouseRef}
+                />;
+              })}
+            </div>
+          )
         })}
       </div>
     </main>
--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
 interface GridProps {
-    coordinate: number[]
+    coordinate: number[],
+    type: string,
+    mouseRef: React.MutableRefObject<boolean>,
+    isVisited?: boolean,
+    isShortestPath?: boolean,
+    onMouseDown?: (coordinate: number[]) => void,
+    onMouseEnter?: (coordinate: number[]) => void,
+    onMouseUp?: () => void
 }
 
-const Grid: React.FC<GridProps> = ({ coordinate }) => {
+const Grid: React.FC<GridProps> = ({ coordinate, type, mouseRef }) => {
     const [x, y] = coordinate;
-    return <div className='grid' id={x + " " + y}></div>;
+
+    return (<div
+        className={type.concat(' grid')}
+        id={x + " " + y}
+
+        onMouseEnter={(event) => {
+            mouseRef ? event.target.classList.add("wall") : "";
+        }}
+    >
+    </div>)
 };
 
 export default Grid;
